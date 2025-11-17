8207abc66e6dea07f5094aa37c3ddcbcbd20f065
Kethavath-Sreekanth <kethavathsreekanth444@gmail.com>
2025-11-17 14:56:25 +0530

    Initial commit

---
+++ src/com/calci/Main.java
@@ -0,0 +1,10 @@
+package com.calci;
+
+public class Main {
+    public static void main(String[] args) {
+    	Calci calc=new SimpleCalculator();
+    	
+    	System.out.println("Addition of two number is:"+calc.addition(12,24));
+    }
+
+}
---
+++ src/com/calci/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package com.calci;
+
+public class SimpleCalculator implements Calci {
+
+	@Override
+	public int addition(int a, int b) {
+		return a+b;
+	}
+
+
+}

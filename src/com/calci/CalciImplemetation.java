35bf0f27821aa3a86f53cc26aaf4cea506a4f962
Kethavath-Sreekanth <kethavathsreekanth444@gmail.com>
2025-11-18 19:00:01 +0530

    THIS IS INITIAL COMMIT

---
+++ src/com/calci/CalciImplemetation.java
@@ -0,0 +1,29 @@
+package com.calci;
+
+public class CalciImplemetation implements Calculater {
+
+	@Override
+	public int addition(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int subtraction(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int multiplication(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int division(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
